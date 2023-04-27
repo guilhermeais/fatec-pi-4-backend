beabb7a276414c17967fb8e21075383ad7e1bd23
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-04-26 21:56:40 -0300

    refactor: utiliza arquivo env

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,7 @@
 import app from './app'
-const PORT = Number(process.env.PORT || 3000)
+import { env } from './config/env'
+
+const PORT = Number(env.PORT || 3000)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
