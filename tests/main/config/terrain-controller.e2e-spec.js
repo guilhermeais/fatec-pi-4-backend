451aa730a4e69dc33dacae7b5fdbc4af7c91f241
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-04-29 00:19:16 -0300

    test(terrain): cria patch /terrains/{id}

--- tests/main/config/terrain-controller.e2e-spec.js
+++ tests/main/config/terrain-controller.e2e-spec.js
@@ -35,6 +35,23 @@ describe('TerrainController e2e tests', () => {
     return user
   }
 
+  async function createTerrain() {
+    const owner = await createUser()
+    const terrain = Terrain.create({
+      name: faker.address.streetName(),
+      locations: [
+        Location.create({
+          latitude: faker.address.latitude(),
+          longitude: faker.address.longitude(),
+        }),
+      ],
+      ownerId: owner.id,
+    })
+
+    await database.ref(`terrains/${terrain.id}`).set(terrain)
+    return terrain
+  }
+
   describe('POST /terrains', () => {
     test('should return 201 with the terrain', async () => {
       const owner = await createUser()
@@ -80,7 +97,7 @@ describe('TerrainController e2e tests', () => {
 
       expect(response.status).toBe(400)
       expect(response.body.error).toEqual('Longitude não informada.')
-    });
+    })
 
     test('should return 400 if user does not exists', async () => {
       const request = {
@@ -88,7 +105,7 @@ describe('TerrainController e2e tests', () => {
         locations: [
           {
             latitude: faker.address.latitude(),
-            longitude:  faker.address.longitude(),
+            longitude: faker.address.longitude(),
           },
         ],
         ownerId: 'any_id',
@@ -100,6 +117,67 @@ describe('TerrainController e2e tests', () => {
       expect(response.body.error).toEqual(
         'Usuário dono do terreno não encontrado.'
       )
+    })
+  })
+
+  describe('PATCH /terrains/{id}', () => {
+    test('should return 200 and update the existing terrain', async () => {
+      const terrain = await createTerrain()
+      const request = {
+        name: faker.address.streetName(),
+      }
+
+      const response = await supertest(app.app)
+        .patch(`/terrains/${terrain.id}`)
+        .send(request)
+
+      expect(response.status).toBe(200)
+
+      const updatedTerrain = (
+        await database.ref(`terrains/${terrain.id}`).get()
+      ).toJSON()
+
+      expect(updatedTerrain.name).toEqual(request.name)
+      expect(updatedTerrain.name).not.toEqual(terrain.name)
+    })
+
+    test('should return 400 if location is invalid', async () => {
+      const terrain = await createTerrain()
+      
+      const request = {
+        name: faker.address.streetName(),
+        locations: [
+          {
+            latitude: faker.address.latitude(),
+            longitude: null,
+          },
+        ],
+      }
+
+      const response = await supertest(app.app).patch('/terrains/'.concat(terrain.id)).send(request)
+
+      expect(response.status).toBe(400)
+      expect(response.body.error).toEqual('Longitude não informada.')
     });
+
+    // test('should return 400 if user does not exists', async () => {
+    //   const request = {
+    //     name: faker.address.streetName(),
+    //     locations: [
+    //       {
+    //         latitude: faker.address.latitude(),
+    //         longitude:  faker.address.longitude(),
+    //       },
+    //     ],
+    //     ownerId: 'any_id',
+    //   }
+
+    //   const response = await supertest(app.app).post('/terrains').send(request)
+
+    //   expect(response.status).toBe(400)
+    //   expect(response.body.error).toEqual(
+    //     'Usuário dono do terreno não encontrado.'
+    //   )
+    // });
   })
 })
