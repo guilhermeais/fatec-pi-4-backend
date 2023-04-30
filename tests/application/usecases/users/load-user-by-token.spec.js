aa7ae6b4635a67c1209c0b1b78313d56e3851f5f
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-04-30 14:34:22 -0300

    test(Authentication): cria caso de uso para buscar usuário pelo token

---
+++ tests/application/usecases/users/load-user-by-token.spec.js
@@ -0,0 +1,65 @@
+import { faker } from '@faker-js/faker'
+import { LoadUserByToken } from '../../../../src/application/usecases/auth/load-user-by-token'
+import { TokenGeneratorSpy } from '../../../mocks/application/interfaces/token-generator.spy'
+import { UserRepositorySpy } from '../../../mocks/application/repositories/user-repository.spy'
+import { vitest } from 'vitest'
+import { UnauthorizedError } from '../../../../src/application/errors'
+
+describe('LoadUserByToken', () => {
+  function makeSut() {
+    const userRepositorySpy = new UserRepositorySpy()
+    const descrypterSpy = new TokenGeneratorSpy()
+
+    const sut = new LoadUserByToken({
+      userRepository: userRepositorySpy,
+      decrypter: descrypterSpy,
+    })
+
+    return {
+      sut,
+      userRepositorySpy,
+      descrypterSpy,
+    }
+  }
+
+  test('should return the decrypted user on success', async () => {
+    const { sut, userRepositorySpy, descrypterSpy } = makeSut()
+
+    const accessToken = faker.datatype.uuid()
+
+    const user = await sut.execute({ accessToken })
+
+    expect(user).toEqual(userRepositorySpy.findByIdResult)
+
+    expect(descrypterSpy.decryptParams).toBe(accessToken)
+    expect(userRepositorySpy.findByIdParams).toBe(
+      descrypterSpy.decryptResult.id
+    )
+  })
+
+  test('should throw UnauthorizedError if the decrypter throws', async () => {
+    const { sut, userRepositorySpy, descrypterSpy } = makeSut()
+    const mockedDecryptError = new Error('decrypt_error')
+    vitest.spyOn(descrypterSpy, 'decrypt').mockRejectedValue(mockedDecryptError)
+
+    const accessToken = faker.datatype.uuid()
+
+    const promise = sut.execute({ accessToken })
+
+    await expect(promise).rejects.toThrow(
+      new UnauthorizedError({
+        message: `Token inválido: ${mockedDecryptError.message}`,
+      })
+    )
+  })
+
+  test('should throw UnauthorizedError if decrypted user id is not found', async () => {
+    const { sut, userRepositorySpy } = makeSut()
+    userRepositorySpy.findByIdResult = null
+    const accessToken = faker.datatype.uuid()
+
+    const promise = sut.execute({ accessToken })
+
+    await expect(promise).rejects.toThrow(new UnauthorizedError())
+  })
+})
--- tests/mocks/application/interfaces/token-generator.spy.js
+++ tests/mocks/application/interfaces/token-generator.spy.js
@@ -5,9 +5,19 @@ export class TokenGeneratorSpy extends TokenGenerator {
   constructor() {
     super()
     this.result = faker.datatype.uuid()
+
+    this.decryptResult = {
+      id: faker.datatype.uuid(),
+    }
   }
   async generate(payload) {
     this.payload = payload
     return this.result
+  }
+
+  async decrypt(token) {
+    this.decryptParams = token;
+
+    return this.decryptResult
   }
 }
