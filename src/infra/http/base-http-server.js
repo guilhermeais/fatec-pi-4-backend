2d61eb2e0322945b573342b23a3a193e696a5f07
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-05-01 11:44:17 -0300

    feat(BaseHTTPServer): adiciona suporte de middlwares na interface do servidor HTTP

--- src/infra/http/base-http-server.js
+++ src/infra/http/base-http-server.js
@@ -1,58 +1,58 @@
-import { NotImplementedError } from '../../application/errors';
+import { NotImplementedError } from '../../application/errors'
 
 export class BaseHTTPServer {
-  #app;
+  #app
 
-  get app () {
-    return this.#app;
+  get app() {
+    return this.#app
   }
 
-  get(path, callback) {
+  get(path, callback, { middlewares = [] }) {
     throw new NotImplementedError({
       methodName: 'get',
       className: this.constructor.name,
-    });
+    })
   }
 
-  post(path, callback) {
+  post(path, callback, { middlewares = [] }) {
     throw new NotImplementedError({
       methodName: 'post',
       className: this.constructor.name,
-    });
+    })
   }
 
-  put(path, callback) {
+  put(path, callback, { middlewares = [] }) {
     throw new NotImplementedError({
       methodName: 'put',
       className: this.constructor.name,
-    });
+    })
   }
 
-  delete(path, callback) {
+  delete(path, callback, { middlewares = [] }) {
     throw new NotImplementedError({
       methodName: 'delete',
       className: this.constructor.name,
-    });
+    })
   }
 
-  patch(path, callback) {
+  patch(path, callback, { middlewares = [] }) {
     throw new NotImplementedError({
       methodName: 'patch',
       className: this.constructor.name,
-    });
+    })
   }
 
   async listen(port, callback) {
     throw new NotImplementedError({
       methodName: 'listen',
       className: this.constructor.name,
-    });
+    })
   }
 
   async close() {
     throw new NotImplementedError({
       methodName: 'close',
       className: this.constructor.name,
-    });
+    })
   }
 }
