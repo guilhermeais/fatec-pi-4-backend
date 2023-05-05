f0420689130d55d2a03564ae9347e04cf59e6c73
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-05-05 19:33:52 -0300

    refactor: remove all terrain domain

--- src/main/config/http-controller.js
+++ src/main/config/http-controller.js
@@ -1,7 +1,6 @@
-import { makeUserController } from '../factories/users/user-controller.factory'
+import { makeUserController } from '../factories/auth/user-controller.factory'
 import { BaseHTTPServer } from '../../infra/http/base-http-server'
-import { makeTerrainController } from '../factories/terrains/terrain-controller.factory'
-import { makeAuthMiddleware } from '../factories/users/auth-middleware.factory'
+import { makeAuthMiddleware } from '../factories/auth/auth-middleware.factory'
 
 export class HttpController {
   constructor({ httpServer = new BaseHTTPServer() }) {
@@ -18,7 +17,6 @@ export class HttpController {
 
   registerRoutes() {
     this.#registerAuthRoutes()
-    this.#registerTerrainRoutes()
   }
 
   #registerAuthRoutes() {
@@ -31,39 +29,6 @@ export class HttpController {
     this.httpServer.post(
       '/auth/sign-in',
       userController.signin.bind(userController)
-    )
-  }
-
-  #registerTerrainRoutes() {
-    const terrainController = makeTerrainController()
-    this.httpServer.get(
-      '/terrains',
-      terrainController.readUserTerrains.bind(terrainController),
-      {
-        middlewares: [makeAuthMiddleware()],
-      }
-    )
-
-    this.httpServer.post(
-      '/terrains',
-      terrainController.createTerrain.bind(terrainController),
-      {
-        middlewares: [makeAuthMiddleware()],
-      }
-    )
-    this.httpServer.patch(
-      '/terrains/:id',
-      terrainController.updateTerrain.bind(terrainController),
-      {
-        middlewares: [makeAuthMiddleware()],
-      }
-    )
-    this.httpServer.delete(
-      '/terrains/:id',
-      terrainController.deleteTerrainById.bind(terrainController),
-      {
-        middlewares: [makeAuthMiddleware()],
-      }
     )
   }
 }
